b6ef10db15696d5a7cf7a84d8049a356e5a1d0db
tusher112 <mdtusherahmed820@gmail.com>
2022-04-22 16:03:36 +0600

    dynamic api create

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello nodemon!!')
+    res.send('Hello nodemon!')
+})
+
+const users=[
+    {id:1,name:'robin',job:'web', email:'abc@xyz.com',phone:'017654321'},
+    {id:2,name:'kabir',job:'data engineer', email:'abc@xyz.com',phone:'017654321'},
+    {id:3,name:'ejabir',job:'marketing', email:'eabc@xyz.com',phone:'017654321'},
+    {id:4,name:'khabir',job:'seo', email:'abc@xyz.com',phone:'017654321'},
+    {id:5,name:'ronbin',job:'co ordinator', email:'abc@xyz.com',phone:'017654321'},
+    {id:6,name:'dobin',job:'hr', email:'abc@xyz.com',phone:'017654321'},
+]
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+
+app.get('/users/:id', (req,res)=>{
+    const id=req.params.id;
+    const user=users.find(u=>u.id==id);
+    res.send(user);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
