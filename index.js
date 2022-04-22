030835883d18a087614afafbefbabc96c5250b90
tusher112 <mdtusherahmed820@gmail.com>
2022-04-22 19:34:49 +0600

    install cors for frtch data from server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(cors())
+
 
 app.get('/', (req, res) => {
     res.send('Hello nodemon!')
